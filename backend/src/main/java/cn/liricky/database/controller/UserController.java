bd67bf4d8da2fdb3f346c45ff5e46cf7a4669181
spirit <right1426@163.com>
2019-02-27 13:07:19 +0800

    新建分支

--- backend/src/main/java/cn/liricky/database/controller/UserController.java
+++ backend/src/main/java/cn/liricky/database/controller/UserController.java
@@ -1,4 +1,8 @@
 package cn.liricky.database.controller;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class UserController {
+
 }
---
+++ backend/src/main/java/cn/liricky/database/model/entity/show/grade.java
@@ -0,0 +1,5 @@
+package cn.liricky.database.model.entity.show;
+
+public class grade {
+
+}
